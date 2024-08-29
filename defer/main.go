84347fb6101a402bfe23f19751f58178f90e1fe6
Bhomik Singh <bhomik009@gmail.com>
2024-08-29 19:03:33 +0530

    defer in go

---
+++ defer/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+// func main() {
+// 	defer fmt.Println("hello")
+// 	// defer statement will be moved to the last of the function execution
+
+// 	fmt.Println("World")
+// }
+
+func main() {
+	defer fmt.Println("World")
+	defer fmt.Println("One")
+	defer fmt.Println("Two")
+	fmt.Println("Hello")
+	myDefer()
+
+}
+
+// world, One, Two
+
+// 0, 1, 2, 3, 4
+// hello, 43210, two, One, world
+
+func myDefer() {
+	for i := 0; i < 5; i++ {
+		defer fmt.Print(i)
+	}
+}
